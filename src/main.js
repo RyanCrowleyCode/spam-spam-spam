a0e1755be5342f004ec468818bcbe25c53138710
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-30 16:04:53 -0500

    exercise completed with nested forEach methods

--- src/main.js
+++ src/main.js
@@ -240,3 +240,18 @@ let customers = [
       }
     }
   ];
+
+//   You have been tasked with building a list of email addresses of all of our customer's contacts so we can spam them with an email about Miffles the Vampire Weiner Dog.
+
+//   From that array, extract just the customers' contact email addresses and store them in a new array. You will need a nested array method - meaning one iteration inside another one - since you need to iterate the entire array of customers, and then iterate the array of emails for each one.
+
+// console.log(customers[0].contacts.email)
+const allEmails = []
+customers.forEach(customer => {
+    customer.contacts.email.forEach(address => {
+        allEmails.push(address)
+    })
+})
+
+
+console.log(allEmails)
